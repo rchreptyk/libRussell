26febf6347a0141ef784cb58b3db30bb707f143e
Russell Chreptyk <russell@Russells-MacBook-Pro.local>
2013-03-29 18:51:30 -0400

    Removed generated headers

--- headers/Boolean.h
+++
@@ -1,8 +0,0 @@
-/* The boolean type definition */
-
-#ifndef bool_type
-#define bool_type
-
-typedef enum { false, true } Boolean;
-
-#endif
--- headers/HashFunctions.h
+++
@@ -1,5 +0,0 @@
-#include <stdlib.h>
-#include "rString.h"
-#include "List.h"
-
-unsigned int shaLikeHash(String *);
